4d83a86f1006c68fca8b7b29f21c584bc3f72db9
aossucci <antonio.ossucci@gmail.com>
2024-03-12 20:41:50 -0300

    Refactor: Change in the code including numbers smaller than 20.

--- Git/teste02.js
+++ Git/teste02.js
@@ -1,11 +1,16 @@
-let arrayNums = [2, 4, 3, 1, 6, 7, 9, 40, 25];
-let array20 = [];
+let arrayNums = [2, 4, 3, 1, 6, 7, 9, 40, 25, 55, 2, 78];
+let arrayMaiores20 = [];
+let arrayMenores20 = []
 
 for (let i = 0; i < arrayNums.length; i++) {
     let num = arrayNums[i];
     if (num >= 20) {
-        array20.push(num);
+        arrayMaiores20.push(num);
+    }
+    else {
+        arrayMenores20.push(num)
     }
 }
 
-console.log("Os números maiores ou igual a 20 são : ", array20)
+console.log("Os números maiores ou igual a 20 são : ", arrayMaiores20)
+console.log("Os números menores que 20 são : ", arrayMenores20)
