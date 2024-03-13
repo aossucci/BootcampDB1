2efd8c172256c5406c3d7928a85e4caa9268bfb8
aossucci <antonio.ossucci@gmail.com>
2024-03-12 21:49:46 -0300

    fix: problems

--- Git/teste02.js
+++ Git/teste02.js
@@ -1,16 +1,16 @@
 let arrayNums = [2, 4, 3, 1, 6, 7, 9, 40, 25, 55, 2, 78];
-let arrayMaiores20 = [];
-let arrayMenores20 = []
+let arrayMai20 = [];
+let arrayMen20 = []
 
 for (let i = 0; i < arrayNums.length; i++) {
     let num = arrayNums[i];
     if (num >= 20) {
-        arrayMaiores20.push(num);
+        arrayMai20.push(num);
     }
     else {
-        arrayMenores20.push(num)
+        arrayMen20.push(num)
     }
 }
 
-console.log("Os números maiores ou igual a 20 são : ", arrayMaiores20)
-console.log("Os números menores que 20 são : ", arrayMenores20)
+console.log("Os números maiores ou igual a 20 são : ", arrayMai20)
+console.log("Os números menores que 20 são : ", arrayMen20)
